4635bbebed2dae484f8e4bc8218b40d1eed4c639
rob587 <robertocamm02@yahoo.com>
2025-07-18 20:27:05 +0200

    random generator email

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
+const number = document.getElementById('inner');
+
 const emailList = []
 
-for (let i=0; i<5; i++){
+for (let i=0; i<10; i++){
     axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((resp)=>{
         emailList.push(resp.data.response);
-        
+        number.innerHTML = emailList
+
     })
 
 }
