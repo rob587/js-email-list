768b68abd6db0cc6fc265407a010c95146c8c76c
rob587 <robertocamm02@yahoo.com>
2025-07-20 12:03:51 +0200

    [upd] modifica estetica codice

--- js/script.js
+++ js/script.js
@@ -4,24 +4,24 @@ const emailList = []
 const button = document.getElementById('btn-generator')
 
 button.addEventListener('click', (e) => {
-    e.preventDefault()
-    const number = document.getElementById('inner');
+  e.preventDefault()
+  const number = document.getElementById('inner');
 
-    for (let i=0; i<10; i++){
-    axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((resp)=>{
-        emailList.push(resp.data.response);
-        number.innerHTML = emailList
+  for (let i = 0; i < 10; i++) {
+    axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((resp) => {
+      emailList.push(resp.data.response);
+      number.innerHTML = emailList.map(email => `<div>${email}</div>`).join('')
 
     })
 
-}
-}) 
+  }
+})
 
 
- document.getElementById("refresh").addEventListener("click", function() {
-    location.reload();
+document.getElementById("refresh").addEventListener("click", function () {
+  location.reload();
 
-  });
+});
 
 
 //   creare e sostituire le 10 email create con altre 10 email randomiche
