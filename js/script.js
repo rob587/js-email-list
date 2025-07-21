e5b56748868278b356141166187bcb388a95ea14
rob587 <robertocamm02@yahoo.com>
2025-07-21 09:40:30 +0200

    [fix]

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ button.addEventListener('click', (e) => {
   for (let i = 0; i < 10; i++) {
     axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((resp) => {
       emailList.push(resp.data.response);
-      number.innerHTML = emailList.map(email => `<div>${email}</div>`).join('')
+      number.innerHTML = emailList.map(email => `<li>${email}</li>`).join('')
 
     })
 
